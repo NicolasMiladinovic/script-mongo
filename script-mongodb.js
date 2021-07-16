34d438fd6e06aef41c5491525bf21b49d3031916
Nicolas Miladinovic <nicolasmiladinovic@mbp-de-nicolas.home>
2021-07-16 12:14:45 +0200

    first commit

---
+++ script-mongodb.js
@@ -0,0 +1,18 @@
+accounts = db.accounts.find().toArray();
+accounts.forEach(account => {
+
+  let accountId = account._id
+  let template_parameters = account.template_parameters
+  let x = template_parameters.find(z => z.name === "showPrices")
+
+  let i = template_parameters.indexOf(x)
+
+  account.priceIsVisible = true
+  if (x.value === "none") {
+    account.priceIsVisible = false
+  }
+
+  let o = account.template_parameters.splice(i, 1)
+
+  db.accounts.updateOne({ _id: accountId }, { $set: account })
+});
